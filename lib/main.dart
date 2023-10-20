6a71f773d8917a196e5e77f7facb50fc103baa34
Omar_Ameer <omar.ameer244@gmail.com>
2023-10-20 18:37:47 +0300

    Testing Localization.

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => LanguageCubit(),
       child: BlocBuilder<LanguageCubit, LanguageState>(
